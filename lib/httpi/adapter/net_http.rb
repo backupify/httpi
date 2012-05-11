65d03c0e402a2cff3c731a931c6a5683415f7184
John Kelly <john@backupify.com>
2012-05-11 15:40:49 -0400

    Allow setting ssl_version

--- lib/httpi/adapter/net_http.rb
+++ lib/httpi/adapter/net_http.rb
@@ -11,6 +11,7 @@ module HTTPI
     class NetHTTP
 
       def initialize(request)
+        request.ssl_version = "SSLv3"
         self.client = new_client request
       end
 
@@ -88,7 +89,6 @@ module HTTPI
         client.cert = ssl.cert
         client.ca_file = ssl.ca_cert_file if ssl.ca_cert_file
         client.verify_mode = ssl.openssl_verify_mode
-        client.ssl_version = ssl.ssl_version
       end
 
       def request_client(type, request)
